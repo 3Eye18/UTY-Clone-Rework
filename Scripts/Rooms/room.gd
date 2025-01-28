5192f762c0a2db05537eb60b1cc03605a39ad44c
Nguyễn Anh Vũ <101244686+3Eye18@users.noreply.github.com>
2025-01-29 02:22:38 +0700

    Setup collisions and set camera limit dynamically

--- Scripts/Rooms/room.gd
+++ Scripts/Rooms/room.gd
@@ -1,8 +1,6 @@
 extends Node2D
 class_name Room
 
-@onready var clover = preload("res://Scenes/Characters/clover.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,5 +11,20 @@ func _process(delta):
 	pass
 
 
-func get_room_size(sprite: Sprite2D):
-	print(sprite.texture.get_size())
+#---------------------------------top---------------------------------
+#|								   |								 |
+#|						    size.y * scale / 2						 |
+#|								   |								 |
+#left -- size.x * scale / 2 -- position -- size.x * scale / 2 -- right
+#|								   |								 |
+#|						    size.y * scale / 2						 |
+#|								   |								 |
+#---------------------------------bot---------------------------------
+func get_room_size(sprite: Sprite2D) -> Array:
+	var room_size = sprite.texture.get_size()
+	var right = sprite.position.x + room_size.x * sprite.scale.x / 2
+	var left = sprite.position.x - room_size.x * sprite.scale.x / 2
+	var bot = sprite.position.y + room_size.y * sprite.scale.y / 2
+	var top = sprite.position.y - room_size.y * sprite.scale.y / 2
+	return [left, top, right, bot]
+	
--- Scripts/Rooms/snowdin_forest.gd
+++ Scripts/Rooms/snowdin_forest.gd
@@ -1,9 +1,12 @@
 extends Room
 
+@onready var clover = preload("res://Scenes/Characters/clover.tscn")
+@onready var spawn_point = %Marker2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_room_size($SnowdinForest)
+	self.add_child(clover.instantiate())
+	self.get_node("Clover").global_position = spawn_point.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +15,24 @@ func _process(delta):
 
 
 func _on_shadow_area_body_entered(body):
-	$Clover.get_node("Sprite2D").material.set_shader_parameter("darken_factor", 0.5)
+	if body.is_in_group("character"):
+		body.get_node("Sprite2D").material.set_shader_parameter("darken_factor", 0.5)
 
 
 func _on_shadow_area_body_exited(body):
-	$Clover.get_node("Sprite2D").material.set_shader_parameter("darken_factor", 1)
+	if body.is_in_group("character"):
+		body.get_node("Sprite2D").material.set_shader_parameter("darken_factor", 1)
+
+
+func _on_child_entered_tree(node):
+	if node == self.get_node("Clover"):
+		var limit_array = get_room_size($SnowdinForest)
+		# Set limit of camera
+		# 0 - left
+		# 1 - top
+		# 2 - right
+		# 3 - bot
+		node.get_node("Camera2D").set_limit(0, limit_array[0])
+		node.get_node("Camera2D").set_limit(1, limit_array[1])
+		node.get_node("Camera2D").set_limit(2, limit_array[2])
+		node.get_node("Camera2D").set_limit(3, limit_array[3])
